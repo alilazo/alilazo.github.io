a4f031efb2e7b7318f11f9ef119285bbacaf450f
Alilazo <lazoali98@gmail.com>
2023-01-25 16:23:41 -0600

    Refactoring

--- js/script.js
+++ js/script.js
@@ -2,42 +2,55 @@
 const isMobile = () => {
     if (window.innerWidth < 768) {
       console.log("Mobile");
-        return true;
+      return true;
     }
+}
+
+// function to show projects
+const showProjects = (projects, start, end) => {
+  for (let i = start; i < end; i++) {
+      projects[i].style.display = "block";
+      console.log("Projects Shown: " + i);
+  }
+}
+
+// function to hide the "load more" button when all projects have been shown
+const hideLoadMoreBtn = (loadMoreBtn, projectsLength, totalProjects) => {
+  if (totalProjects >= projectsLength) {
+      loadMoreBtn.classList.add("hide");
+  }
 }
 
 
 //Load more projects btn
 function loadMoreProjects () {
-    var porjectsOnScreen = 6;
-    var loadMoreBtn = document.querySelector(".loadMoreBtn");
-    var projects = document.querySelectorAll(".project");
-    var projectsLength = projects.length;
-    var projectsToShow = porjectsOnScreen;
-    var projectsShown = porjectsOnScreen;
-    var totalProjects =  projectsShown + projectsToShow;
+    const porjectsOnScreen = 6;
+    const loadMoreBtn = document.querySelector(".loadMoreBtn");
+    const projects = document.querySelectorAll(".project");
+    const projectsLength = projects.length;
+    let projectsToShow = porjectsOnScreen;
+    let projectsShown = porjectsOnScreen;
+    const totalProjects =  projectsShown + projectsToShow;
 
-    if (totalProjects >= projectsLength)
-        loadMoreBtn.classList.add("hide");
-
-    for (var i = projectsShown; i < projectsLength; i++) {
-        projects[i].style.display = "block";
-        console.log("Projects Shown: " + i);
-    }
+    hideLoadMoreBtn(loadMoreBtn, projectsLength, totalProjects);
+    showProjects(projects, projectsShown, projectsToShow);
 }
 
 //Navigation bar autoHideScroll
 const autoHideScroll = () => {
-    autohide = document.querySelector('.autohide');
-    navEnabler = document.querySelector('.navig');
-    navbar_height = document.querySelector('.navbar').offsetHeight;
+    const autohide = document.querySelector('.autohide');
+    const navEnabler = document.querySelector('.navig');
+    const navbar_height = document.querySelector('.navbar').offsetHeight;
+    
     document.body.style.paddingTop = navbar_height + 'px';
 
     if(autohide){
-        var last_scroll_top = 0;
+        let last_scroll_top = 0;
+        
         window.addEventListener('scroll', function() {
             navEnabler.classList.remove('navig');
-            let scroll_top = window.scrollY;
+            const scroll_top = window.scrollY;
+            
             if(scroll_top < last_scroll_top) {
                 autohide.classList.remove('scrolled-down');
                 autohide.classList.add('scrolled-up');
@@ -53,25 +66,22 @@ const autoHideScroll = () => {
 //When the screen leads start the animations
 const onLoadFunction = () => { 
     autoHideScroll();
-    var autohide = document.querySelector('.autohide');
-    var headerText = document.querySelector(".headerTitleText");
-    var subHeaderText = document.querySelector(".subHeaderText");
+    const autohide = document.querySelector('.autohide');
+    const headerText = document.querySelector(".headerTitleText");
+    const subHeaderText = document.querySelector(".subHeaderText");
 
     headerText.classList.add("active");
     autohide.classList.add('scrolled-down');
-    headerText.addEventListener("transitionend", function() {
+    headerText.addEventListener("transitionend", () => {
         subHeaderText.classList.add("active");
     });
 }
 
-window.addEventListener("load", () => onLoadFunction());
-
-
 //reveal class scroll animation
 const reveal = () => {
-    var reveals = document.querySelectorAll(".reveal");
+    const reveals = document.querySelectorAll(".reveal");
     for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
+      const windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
       var elementVisible = 150;
       if (elementTop < windowHeight - elementVisible) {
@@ -83,22 +93,23 @@ const reveal = () => {
 }
 
 //reveal class scroll animation
-const revealLR = () => {
-    var reveals = document.querySelectorAll(".revealLR");
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
+const revealLR = (className) => {
+    const reveals = document.querySelectorAll('${className}');
+    const elementVisible = 150;
+    const windowHeight = window.innerHeight;
+
+    reveals.forEach((reveal) => {
+      var elementTop = reveal.getBoundingClientRect().top;
+      
       if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
+        reveal.classList.add("active");
       } else {
-        reveals[i].classList.remove("active");
+        reveal.classList.remove("active");
       }
-    }
+    })
 }
 
 isMobile();
-window.addEventListener("scroll", reveal);
-window.addEventListener("scroll", revealLR);
-reveal();
-revealLR();
+window.addEventListener("load", () => onLoadFunction());
+window.addEventListener("scroll", () => reveal("reveal"));
+window.addEventListener("scroll", () => reveal("revealLR"));
