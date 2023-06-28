511fcbe75f2d6699a74ccddd11334bdc8c48fde8
alilazo <lazoali98@gmail.com>
2023-06-28 00:53:25 -0500

    Added TypeWriter

--- js/script.js
+++ js/script.js
@@ -66,6 +66,7 @@ const autoHideScroll = () => {
 //When the screen leads start the animations
 const onLoadFunction = () => { 
     autoHideScroll();
+    
     const autohide = document.querySelector('.autohide');
     const headerText = document.querySelector(".headerTitleText");
     const subHeaderText = document.querySelector(".subHeaderText");
@@ -77,20 +78,52 @@ const onLoadFunction = () => {
     headerText.addEventListener("transitionend", () => {
         subHeaderText.classList.add("active");
     });
+    const typed = new Typed('.typewriter', {
+        strings: ['As a senior Computer Science student at Texas A&amp;M-Corpus Christi, I am eager to transition into a full-time role in the field. With a passion for programming and a constant desire to learn and improve, I am confident in my ability to contribute to a company\'s success. My 21 years of experience and hardworking attitude make me a valuable asset to any team. I have a strong background in Java, C++, HTML, CSS, JavaScript and have worked with front-end and back-end frameworks such as MongoDB, ExpressJS, ReactJS, and NodeJS through my experience at IBM during my Accelerate Program.'],
+        typeSpeed: 8, // Adjust the typing speed (in milliseconds)
+        loop: false, // Set to true if you want the typewriter effect to loop
+        cursorChar: '|',
+    });
 }
+
+/*
+//write a function that checks if the element ID "About" has a class called "active" and if it does then start the typed animation
+const startTypeOnAbout = () => {
+    let hasStartedTyping = false;
+    const about = document.querySelector("#About");
+
+    if(about.classList.contains("active") && !hasStartedTyping){
+      hasStartedTyping = true;
+      const typed = new Typed('.typewriter', {
+        strings: ['As a senior Computer Science student at Texas A&amp;M-Corpus Christi, I am eager to transition into a full-time role in the field. With a passion for programming and a constant desire to learn and improve, I am confident in my ability to contribute to a company\'s success. My 21 years of experience and hardworking attitude make me a valuable asset to any team. I have a strong background in Java, C++, HTML, CSS, JavaScript and have worked with front-end and back-end frameworks such as MongoDB, ExpressJS, ReactJS, and NodeJS through my experience at IBM during my Accelerate Program.'],
+        typeSpeed: 8, // Adjust the typing speed (in milliseconds)
+        loop: false, // Set to true if you want the typewriter effect to loop
+        cursorChar: '|',
+      });
+      console.log("Typing");
+      console.log(hasStartedTyping);
+      typed.start();
+    } else if (hasStartedTyping) {
+      console.log("Stopped Typing");
+      console.log(hasStartedTyping);
+    }
+} */
 
 //reveal class scroll animation
 const reveal = () => {
     const reveals = document.querySelectorAll(".reveal");
+
     for (var i = 0; i < reveals.length; i++) {
       const windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
       var elementVisible = 150;
+
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
       } else {
         reveals[i].classList.remove("active");
       }
+
     }
 }
 
@@ -115,3 +148,5 @@ isMobile();
 window.addEventListener("load", () => onLoadFunction());
 window.addEventListener("scroll", () => reveal("reveal"));
 window.addEventListener("scroll", () => reveal("revealLR"));
+//startTypeOnAbout is called in the reveal function because it checks if the element has the class "active"
+window.addEventListener("scroll", () => startTypeOnAbout());
